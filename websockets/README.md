4b20caabaf12849ff99443646c53e00d74e17e98
Gid <giddevanderven@gmail.com>
2020-03-20 09:36:03 +0100

    fix typo

---
+++ websockets/README.md
@@ -0,0 +1,7 @@
+# Open Api WebSocket Streaming
+
+### C# Sample
+
+This is a sample console application that creates a price subscription and sets up a WebSocket connection to stream those prices.
+
+Open solution in Visual Studio, replace the ##### token with the one you have retrieved through our developer portal at https://developer.saxo and press F5 to run.
---
+++ websockets/Streaming.WebSocket.Samples/WebSocketMessage.cs
@@ -0,0 +1,30 @@
+﻿namespace Streaming.WebSocket.Samples
+{
+	/// <summary>
+	/// The WebSocketMessage is the data envelope returned by the streaming server. 
+	/// The message contains meta data describing the message and where it comes from.
+	/// Also included is the actual data, in the Payload property. 
+	/// </summary>
+	public class WebSocketMessage
+	{
+		/// <summary>
+		/// Message id that is unique within a streaming session.
+		/// </summary>
+		public long MessageId { get; set; }
+
+		/// <summary>
+		/// A unique subscription identifier. This is the reference id provided when the subscription was created.
+		/// </summary>
+		public string ReferenceId { get; set; }
+		
+		/// <summary>
+		/// The message payload format. This is always Json, which has the value 0.
+		/// </summary>
+		public int PayloadFormat { get; set; }
+
+		/// <summary>
+		/// The payload is the actual data message. For Json this is a byte array representation of a UTF-8 encoded string.
+		/// </summary>
+		public byte[] Payload { get; set; }
+	}
+}
