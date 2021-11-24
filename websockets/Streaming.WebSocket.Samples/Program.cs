ec8a4b2c6d5d28451e8add625afcdeb7d4c7cf63
bas <bas.groot@basement.nl>
2021-11-24 16:21:35 +0100

    Upgrade to .NET 4.8

--- websockets/Streaming.WebSocket.Samples/Program.cs
+++ websockets/Streaming.WebSocket.Samples/Program.cs
@@ -4,78 +4,78 @@ using System.Threading.Tasks;
 
 namespace Streaming.WebSocket.Samples
 {
-	/// <summary>
-	/// Sample runner.
-	/// </summary>
-	class Program
-	{
-		private static Action _closeConnectionAction;
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Press ESC to disconnect.");
+    /// <summary>
+    /// Sample runner.
+    /// </summary>
+    class Program
+    {
+        private static Action _closeConnectionAction;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press ESC to disconnect.");
 
-			Task taskKeys = new Task(ListenForEscape);
+            Task taskKeys = new Task(ListenForEscape);
 
-			CancellationTokenSource cts = new CancellationTokenSource();
-			Task taskRunSample = new Task(async () => { await RunSample(cts); });
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Task taskRunSample = new Task(async () => { await RunSample(cts); });
 
-			taskKeys.Start();
-			try
-			{
-				taskRunSample.Start();
-			}
-			catch (TaskCanceledException)
-			{
-				taskRunSample.Dispose();
-			}
+            taskKeys.Start();
+            try
+            {
+                taskRunSample.Start();
+            }
+            catch (TaskCanceledException)
+            {
+                taskRunSample.Dispose();
+            }
 
-			Task.WaitAll(taskKeys);
-			
-			cts.Cancel();
-			taskRunSample.Wait(cts.Token);
-			Console.WriteLine("Press ESC to close window.");
-			Console.ReadKey();
-		}
+            Task.WaitAll(taskKeys);
+            
+            cts.Cancel();
+            taskRunSample.Wait(cts.Token);
+            Console.WriteLine("Press ESC to close window.");
+            Console.ReadKey();
+        }
 
-		/// <summary>
-		/// Run the sample and set up a callback to handle Web Socket close on ESC.
-		/// </summary>
-		/// <param name="cts"></param>
-		private static async Task RunSample(CancellationTokenSource cts)
-		{
-			WebSocketSample sample = new WebSocketSample();
-			async void CloseConnectionCallback() => await sample.StopWebSocket();
-			_closeConnectionAction = CloseConnectionCallback;
+        /// <summary>
+        /// Run the sample and set up a callback to handle Web Socket close on ESC.
+        /// </summary>
+        /// <param name="cts"></param>
+        private static async Task RunSample(CancellationTokenSource cts)
+        {
+            WebSocketSample sample = new WebSocketSample();
+            async void CloseConnectionCallback() => await sample.StopWebSocket();
+            _closeConnectionAction = CloseConnectionCallback;
 
-			try
-			{
-				await sample.RunSample(cts);
-			}
-			finally
-			{
-				sample.Dispose();
-				cts.Cancel();
-			}
+            try
+            {
+                await sample.RunSample(cts);
+            }
+            finally
+            {
+                sample.Dispose();
+                cts.Cancel();
+            }
 
-			Console.WriteLine("Stopped sample.");
-		}
+            Console.WriteLine("Stopped sample.");
+        }
 
-		#region Input handling
-		/// <summary>
-		/// Listen for keyboard input from user.
-		/// </summary>
-		private static void ListenForEscape()
-		{
+        #region Input handling
+        /// <summary>
+        /// Listen for keyboard input from user.
+        /// </summary>
+        private static void ListenForEscape()
+        {
             while (!Console.KeyAvailable)
-			{
+            {
                 ConsoleKeyInfo key = Console.ReadKey(true);
                 if (key.Key == ConsoleKey.Escape)
-				{
-					_closeConnectionAction?.Invoke();
-					break;
-				}
-			}
-		}
-		#endregion 
-	}
+                {
+                    _closeConnectionAction?.Invoke();
+                    break;
+                }
+            }
+        }
+        #endregion 
+    }
 }
