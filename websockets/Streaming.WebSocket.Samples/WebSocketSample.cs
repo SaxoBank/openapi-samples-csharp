ff5ee37af4557829c87c579a12f5aab2746af2ef
ayxue <ngsbaoyy@hotmail.com>
2021-12-02 17:32:28 +0800

    remove redundant using

--- websockets/Streaming.WebSocket.Samples/WebSocketSample.cs
+++ websockets/Streaming.WebSocket.Samples/WebSocketSample.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
